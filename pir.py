12aedfcf09ea2d5e7236b7621859591f92826263
Zechen Wang <zechenwa@usc.edu>
2022-11-29 15:17:03 -0500

    debug changes

--- pir.py
+++ pir.py
@@ -12,14 +12,14 @@ GPIO.setup(PIR_1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 def motion(channel):
 	message = 'motion detected'
 	curr_time = datetime.datetime.now().strftime("%H:%M:%S %d/%m/%Y")
-	resp = requests.post('https://textbelt.com/text', {
-		'phone': config.PHONE,
-		'message': message + " at " + curr_time,
-		'key': config.KEY,
-	})
+	# resp = requests.post('https://textbelt.com/text', {
+	# 	'phone': config.PHONE,
+	# 	'message': message + " at " + curr_time,
+	# 	'key': config.KEY,
+	# })
 	# debug uses
 	print(message + " at " + curr_time)
-	print(resp.json())
+	# print(resp.json())
 	
 GPIO.add_event_detect(PIR_1, GPIO.RISING, callback=motion)
 
