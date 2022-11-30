9f824e772ac4d52e997ab33b22d2930a15ba2a77
Zechen Wang <zechenwang@hotmail.com>
2022-11-30 17:35:06 -0500

    added impulse detection

--- mic.py
+++ mic.py
@@ -10,9 +10,8 @@ duration = 2  # seconds
 myrecording = sd.rec(duration * fs, samplerate=fs, channels=2, blocking=True)
 
 # collect output
-y1 = myrecording[:,0]
+y1 = abs(myrecording[:,0])
 y2 = myrecording[:,1]
-ylen = len(y1)
 
 # calculate avg
 y1_avg = np.average(y1)
@@ -20,10 +19,24 @@ print("avg = " +str(y1_avg))
 
 # compute fft, apply band pass filter, then ifft
 y1_fft = rfft(y1)
+ylen = len(y1_fft)
 y1_fft = [0]*int(0.1*ylen) + list(y1_fft[int(0.1*ylen):int(0.9*ylen)]) + [0]*int(0.1*ylen)
-y1_hp = irfft(y1_fft)
+y1_hp = abs(irfft(y1_fft))
 
-plt.plot(y1, linestyle = 'dotted', label='original')
+y1_hp_avg = np.average(y1_hp)
+
+# detect frequency
+counter = 0
+for i in range (len(y1_hp)):
+	if (y1_hp[i] > (50 * y1_hp_avg)):
+		counter = counter + 1
+	if (counter > 2):
+		print("detected")
+		break
+
+
+
+# plt.plot(y1, linestyle = 'dotted', label='original')
 plt.plot(y1_hp, linestyle = 'dotted', label='highpass')
 # plt.plot(y2, linestyle = 'dotted')
 plt.show()
